311c42aea4b6bd79f93085096bdeb06b9a3db755
RuriYS <ruriyoshinova@proton.me>
2025-06-17 05:44:07 +0000

    refractor(client): fix log messages

--- internal/client.go
+++ internal/client.go
@@ -11,8 +11,11 @@ import (
 )
 
 func StartClient() {
-	slog.Info("dynaport is alive!")
+	slog.Info("[StartClient] dynaport is alive!")
+	handleBroadcast()
+}
 
+func handleBroadcast() {
 	serverAddr := net.UDPAddr{
 		IP:   net.ParseIP(config.Client.Host),
 		Port: int(config.Client.Port),
@@ -20,12 +23,12 @@ func StartClient() {
 
 	broadcastPeriod, err := time.ParseDuration(config.Client.BroadcastInterval)
 	if err != nil {
-		slog.Error("failed to parse broadcast_interval", "StartClient", err.Error())
+		slog.Error("[handleBroadcast] failed to parse broadcast_interval", "error", err.Error(), "broadcast_interval", config.Client.BroadcastInterval)
 	}
 
 	timeout, err := time.ParseDuration(config.Client.Timeout)
 	if err != nil {
-		slog.Error("failed to parse timeout", "StartClient", err.Error())
+		slog.Error("[handleBroadcast] failed to parse timeout", "error", err.Error(), "timeout", config.Client.Timeout)
 	}
 
 	for {
@@ -38,7 +41,7 @@ func StartClient() {
 		processAllocations(conn, allocations, timeout)
 
 		conn.Close()
-		slog.Info(fmt.Sprintf("broadcast complete, sleeping for %v", broadcastPeriod))
+		slog.Info(fmt.Sprintf("[handleBroadcast] broadcast complete, sleeping for %v", broadcastPeriod))
 		time.Sleep(broadcastPeriod)
 	}
 }
@@ -46,11 +49,11 @@ func StartClient() {
 func establishConnection(serverAddr *net.UDPAddr, broadcastPeriod time.Duration) *net.UDPConn {
 	conn, err := net.DialUDP("udp", nil, serverAddr)
 	if err != nil {
-		slog.Error("failed to connect", "establishConnection", err.Error())
+		slog.Error("[establishConnection] failed to connect", "error", err.Error())
 		time.Sleep(broadcastPeriod)
 		return nil
 	}
-	slog.Debug(fmt.Sprintf("connected to %v", serverAddr))
+	slog.Debug("[establishConnection] connected", "serverAddr", serverAddr, "broadcastPeriod", broadcastPeriod)
 	return conn
 }
 
@@ -68,7 +71,7 @@ func processAllocations(conn *net.UDPConn, allocations []types.Allocation, timeo
 		binary.BigEndian.PutUint16(packet[1:], alloc.Port)
 		_, err := conn.Write(packet)
 		if err != nil {
-			slog.Error(fmt.Sprintf("failed to send packet for %s %d", alloc.Protocol, alloc.Port), "processAllocations", err.Error())
+			slog.Error("[processAllocations] failed to send packet", "error", err.Error(), "packet", packet)
 			continue
 		}
 
@@ -76,15 +79,15 @@ func processAllocations(conn *net.UDPConn, allocations []types.Allocation, timeo
 		resp := make([]byte, 1)
 		n, _, err := conn.ReadFromUDP(resp)
 		if err != nil {
-			slog.Warn(fmt.Sprintf("no response for %s %d", alloc.Protocol, alloc.Port), "processAllocations", err.Error())
+			slog.Warn("[processAllocations] timed out", "error", err.Error())
 			continue
 		}
 
 		res := resp[:n]
 		if len(res) == 1 && res[0] == byte(types.OK) {
-			slog.Info(fmt.Sprintf("forwarded port %s %d", alloc.Protocol, alloc.Port))
+			slog.Info("[processAllocations] port forwarded", "port", alloc.Port, "protocol", alloc.Protocol)
 		} else if len(res) == 1 && res[0] == byte(types.Allocated) {
-			slog.Warn(fmt.Sprintf("port already allocated %s %d", alloc.Protocol, alloc.Port))
+			slog.Debug("[processAllocations] port already allocated", "port", alloc.Port, "protocol", alloc.Protocol)
 		}
 	}
 }
